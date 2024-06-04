c17a08079fa14b1b53b7c6960b2bf51152e1c039
Juan Sebastian <juanse.0568@gmail.com>
2024-06-03 19:33:55 -0500

    Vista pedidos de recolector mejorado

--- src/components/ordersInfo/OrderItem.jsx
+++ src/components/ordersInfo/OrderItem.jsx
@@ -5,26 +5,35 @@ import { Feather, Ionicons, FontAwesome } from "@expo/vector-icons";
 
 const OrderItem = ({ order }) => {
   return (
-    <View style={styles.orderItemContainer}>
-      <View style={styles.orderDetails}>
-        <Feather name="shopping-bag" size={40} color="#FFFFFF" />
+    <View style={styles.container}>
+      <View style={styles.orderItemContainer}>
+        <View style={styles.orderDetails}>
+          <Feather name="shopping-bag" size={40} color="#FFFFFF" />
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.orderNumber}># {order.orderNumber}</Text>
+          <Text style={styles.orderTime}>
+            <Ionicons name="time-outline" size={24} color="#FFFFF" />{" "}
+            {order.time}
+          </Text>
+        </View>
+        <TouchableOpacity style={styles.orderButton}>
+          <Text style={styles.orderButtonText}>
+            <FontAwesome name="arrow-right" size={30} color="#FFFFFF" />
+          </Text>
+        </TouchableOpacity>
       </View>
-      <View style={styles.textContainer}>
-        <Text style={styles.orderNumber}># {order.orderNumber}</Text>
-        <Text style={styles.orderTime}>
-          <Ionicons name="time-outline" size={24} color="#FFFFF" /> {order.time}
-        </Text>
-      </View>
-      <TouchableOpacity style={styles.orderButton}>
-        <Text style={styles.orderButtonText}>
-          <FontAwesome name="arrow-right" size={30} color="#FFFFFF" />
-        </Text>
-      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    paddingBottom: 5,
+    backgroundColor: theme.colors.backgroundMain,
+    marginBottom: 5,
+    borderRadius: 10,
+  },
   orderItemContainer: {
     backgroundColor: theme.colors.backgroundCards,
     flexDirection: "row",
@@ -32,7 +41,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     padding: 15,
     borderRadius: 10,
-    marginVertical: 5,
   },
   orderDetails: {
     flexDirection: "column",
@@ -44,6 +52,8 @@ const styles = StyleSheet.create({
   },
   orderTime: {
     color: theme.colors.textPrimary,
+    justifyContent: "center",
+    alignItems: "center",
     fontSize: theme.fonts.sizes.paragraph,
   },
   // orderButton: {
