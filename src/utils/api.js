07ed06087f0b23c94bc5826ec8a1dc7ef56fb869
FelipeCarrr <afnavarroc@ufpso.edu.co>
2025-02-08 15:33:43 -0500

    Login APP mobile

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 const DOMAIN = process.env.EXPO_PUBLIC_DOMAIN;
 const PORT = process.env.EXPO_PUBLIC_PORT;
 const api = {
-  auth: `${DOMAIN}${PORT}/authStaff/login`,
+  auth: `${DOMAIN}${PORT}/authStaff/loginMobile`,
 };
 
 export default api;
